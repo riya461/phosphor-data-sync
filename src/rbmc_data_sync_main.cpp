aaae2950a2f15fe0249d52e04caa73045e9cc90d
Arya K Padman <aryakpadman@gmail.com>
2026-01-06 03:06:06 -0600

    Exiting Data Sync daemon if no config present
    In the current logic, the Data Sync doesn't explicitly
    handle the case where the data-sync configuration might be
    empty or missing.
    
    The two cases which should be handles is when:
    - The data sync configuration directory doesn't exists.
    - If it exists, then it is empty.
    This change ensures Data Sync exits when the configuration
    directory does not exist or is empty.
    
    Change-Id: I2067d2319b74c6686ea93d14cc7b1e50e7f0b5e7
    Signed-off-by: Riya Sabu <riya.sabu461@gmail.com>

--- src/rbmc_data_sync_main.cpp
+++ src/rbmc_data_sync_main.cpp
@@ -11,8 +11,12 @@
 #include <sdbusplus/server/manager.hpp>
 #include <xyz/openbmc_project/Control/SyncBMCData/common.hpp>
 
+#include <filesystem>
+
 int main()
 {
+    namespace fs = std::filesystem;
+
     using SyncBMCData =
         sdbusplus::common::xyz::openbmc_project::control::SyncBMCData;
 
@@ -27,6 +31,15 @@ int main()
             "Caught exception while setting up persistent paths, Err : {ERROR}",
             "ERROR", exc);
         exit(EXIT_FAILURE);
+    }
+
+    if (!fs::exists(DATA_SYNC_CONFIG_DIR) || fs::is_empty(DATA_SYNC_CONFIG_DIR))
+    {
+        const fs::path configDirPath{DATA_SYNC_CONFIG_DIR};
+        lg2::error(
+            "Exiting data-sync, no configurations present in directory {CONFIG_DIR}",
+            "CONFIG_DIR", configDirPath);
+        return EXIT_FAILURE;
     }
 
     sdbusplus::async::context ctx;
