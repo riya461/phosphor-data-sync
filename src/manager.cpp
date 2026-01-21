f3057a24b49cb1989ad170ea91fa2a854dd95ae1
Riya Sabu <riya.sabu461@gmail.com>
2026-01-20 23:48:14 -0600

    Adding scope_fail
    Change-Id: I5a8c4ee535ae4641c8c25d814871e77219c1d0f9
    Signed-off-by: Riya Sabu <riya.sabu461@gmail.com>

--- src/manager.cpp
+++ src/manager.cpp
@@ -693,6 +693,23 @@ sdbusplus::async::task<>
 {
     try
     {
+        using std::experimental::scope_fail;
+        scope_fail{ [&dataSyncCfg, this]() noexcept -> sdbusplus::async::task<> {
+            lg2::error("monitorDataToSync failed for path");
+            // Error log if fails to create watcher for a
+            // file/directory.
+            ext_data::AdditionalData additionalDetails = {
+                {"DS_Sync_Event_Path", dataSyncCfg._path.string()},
+                {"DS_Sync_Event_Msg", "Failed to create watcher for the path"}};
+            additionalDetails["DS_Sync_Event_Type"] =
+                dataSyncCfg.getSyncTypeInStr();
+            co_await _extDataIfaces->createErrorLog(
+                "xyz.openbmc_project.RBMC_DataSync.Error.SyncEventsFailure",
+                ext_data::ErrorLevel::Warning, additionalDetails);
+        } };
+        
+
+
         uint32_t eventMasksToWatch = IN_CLOSE_WRITE | IN_MOVE | IN_DELETE_SELF;
         if (dataSyncCfg._isPathDir)
         {
@@ -707,6 +724,8 @@ sdbusplus::async::task<>
         watch::inotify::DataWatcher dataWatcher(
             _ctx, IN_NONBLOCK, eventMasksToWatch, dataSyncCfg._path,
             excludeList, dataSyncCfg._includeList);
+
+        throw std::runtime_error("Test exception");
 
         while (!_ctx.stop_requested() && !_syncBMCDataIface.disable_sync())
         {
@@ -730,19 +749,7 @@ sdbusplus::async::task<>
                    "{ERROR}",
                    "PATH", dataSyncCfg._path, "ERROR", e.what());
 
-        // Error log if fails to create watcher for a
-        // file/directory.
-        ext_data::AdditionalData additionalDetails = {
-            {"DS_Sync_Event_Path", dataSyncCfg._path.string()},
-            {"DS_Sync_Event_Msg", "Failed to create watcher for the path"}};
-        additionalDetails["DS_Sync_Event_Type"] =
-            dataSyncCfg.getSyncTypeInStr();
-        if (!_ctx.stop_requested())
-        {
-            _ctx.spawn(_extDataIfaces->createErrorLog(
-                "xyz.openbmc_project.RBMC_DataSync.Error.SyncEventsFailure",
-                ext_data::ErrorLevel::Warning, additionalDetails));
-        }
+        
     }
     co_return;
 }
